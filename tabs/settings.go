cc85cbfdfb693472f61a6ab3efd1a86e93214e07
sharkmu <csonka.samuel@gmail.com>
2025-10-18 16:44:09 +0200

    better placeholder text

--- tabs/settings.go
+++ tabs/settings.go
@@ -2,6 +2,7 @@ package tabs
 
 import (
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/joho/godotenv"
@@ -19,7 +20,27 @@ func OpenSettings(w fyne.Window) fyne.CanvasObject {
 	pathLabel := widget.NewLabel("JSON's folder location: ")
 
 	pathEntry := widget.NewEntry()
-	pathEntry.SetPlaceHolder("Nothing selected")
+
+	configPath, err := utils.GetConfigDir()
+	if err != nil {
+		log.Fatal("Unable to get config directory:", err)
+	}
+	envPath := filepath.Join(configPath, ".env")
+	var placeholderText string
+
+	_, err = os.Stat(envPath)
+	if os.IsNotExist(err) {
+		placeholderText = "Nothing selected"
+	} else if err != nil {
+		log.Fatal("Error checking .env file:", err)
+	} else {
+		err := godotenv.Load(envPath)
+		if err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
+		placeholderText = os.Getenv("JSON_FOLDER_PATH")
+	}
+	pathEntry.SetPlaceHolder(placeholderText)
 
 	browseBtn := widget.NewButton("Browse", func() {
 		fd := dialog.NewFolderOpen(func(r fyne.ListableURI, err error) {
