9dbe1343e1caa585f526a5cd05e76388ffb6225e
sharkmu <csonka.samuel@gmail.com>
2025-10-19 14:22:47 +0200

    better path entry UI

--- tabs/settings.go
+++ tabs/settings.go
@@ -26,11 +26,10 @@ func OpenSettings(w fyne.Window) fyne.CanvasObject {
 		log.Fatal("Unable to get config directory:", err)
 	}
 	envPath := filepath.Join(configPath, ".env")
-	var placeholderText string
 
 	_, err = os.Stat(envPath)
 	if os.IsNotExist(err) {
-		placeholderText = "Nothing selected"
+		pathEntry.SetPlaceHolder("Nothing selected")
 	} else if err != nil {
 		log.Fatal("Error checking .env file:", err)
 	} else {
@@ -38,9 +37,14 @@ func OpenSettings(w fyne.Window) fyne.CanvasObject {
 		if err != nil {
 			log.Fatal("Error loading .env file:", err)
 		}
-		placeholderText = os.Getenv("JSON_FOLDER_PATH")
+		pathEntry.Text = os.Getenv("JSON_FOLDER_PATH")
 	}
-	pathEntry.SetPlaceHolder(placeholderText)
+
+	saveBtn := widget.NewButton("Save", func() {
+		utils.SaveToEnv(pathEntry.Text)
+	})
+
+	pathInput := container.NewBorder(nil, nil, nil, saveBtn, pathEntry)
 
 	browseBtn := widget.NewButton("Browse", func() {
 		fd := dialog.NewFolderOpen(func(r fyne.ListableURI, err error) {
@@ -53,7 +57,7 @@ func OpenSettings(w fyne.Window) fyne.CanvasObject {
 		fd.Show()
 	})
 
-	pathBox := container.NewVBox(pathLabel, pathEntry, browseBtn)
+	pathBox := container.NewVBox(pathLabel, pathInput, browseBtn)
 
 	return container.NewVBox(title, pathBox)
 }
